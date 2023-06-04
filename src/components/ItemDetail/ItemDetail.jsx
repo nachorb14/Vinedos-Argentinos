def317e5f861ccbf2d9f0a5908fd71d8b396463c
Ignacio Romero Barolin <nachorb2001@gmail.com>
2023-06-03 22:44:49 -0300

    Agregando contador a los productos

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,27 @@
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const ItemDetail = ({id, nombre, precio, img}) => {
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+
+  const [agregarCantidad, setAgregarCantidad] = useState(0);
+
+  const handlerCantidad = (cantidad) => {
+    setAgregarCantidad(cantidad);
+    console.log("Productos Agregados: " + cantidad);
+  }
+
   return (
     <div className='contenedorItem'>
-        <h2>Nombre: {nombre} </h2>
-        <h3>Precio: {precio} </h3>
-        <h3>ID: {id} </h3>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio eveniet facere maiores quo tempore quisquam! Consectetur dolores quos ducimus maiores quam quae, eveniet voluptatibus beatae, nemo cumque tempore modi?</p>
-        <img src= {img} alt={nombre} />
+      <h2>Nombre: {nombre} </h2>
+      <h3>Precio: {precio} </h3>
+      <h3>ID: {id} </h3>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga odio eveniet facere maiores quo tempore quisquam! Consectetur dolores quos ducimus maiores quam quae, eveniet voluptatibus beatae, nemo cumque tempore modi?</p>
+      <img src={img} alt={nombre} />
+      {
+        agregarCantidad > 0 ? (<Link to="/cart"> Terminar Compra </Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={handlerCantidad} />)
+      }
     </div>
   )
 }
--- src/components/asyncmock.js
+++ src/components/asyncmock.js
@@ -1,12 +1,12 @@
 const productos = [
-    {nombre: "Elementos Cabernet Sauvignon", precio: 960, id: "1", img: "../img/elementos-cabernet.jpg", idCat: "1"},
-    {nombre: "Don David Cabernet Sauvignon", precio: 1900, id: "2", img: "../img/don-david-cabernet.jpg", idCat: "1"},
-    {nombre: "Navarro Correas Colección Privada Malbec", precio: 2200, id: "3", img: "../img/navarro-correas-malbec.jpg", idCat: "2"},
-    {nombre: "Fond de Cave Reserva Malbec", precio: 2130, id: "4", img: "../img/fond-de-cave-reserva-malbec.jpg", idCat: "2"},
-    {nombre: "Rutini Merlot", precio: 4375, id: "5", img: "../img/rutini-merlot.jpg", idCat: "3"},
-    {nombre: "Chateau Vieux Merlot", precio: 3650, id: "6", img: "../img/chateau-vieux-merlot.jpg", idCat: "3"},
-    {nombre: "Demencial Pinot Noir", precio: 3300, id: "7", img: "../img/demencial-pinot-noir.jpg", idCat: "4"},
-    {nombre: "Luigi Bosca Pinot Noir", precio: 4450, id: "8", img: "../img/luigi-bosca-pinot-noir.jpg", idCat: "4"}    
+    {nombre: "Elementos Cabernet Sauvignon", precio: 960, stock: 10, id: "1", img: "../img/elementos-cabernet.jpg", idCat: "1"},
+    {nombre: "Don David Cabernet Sauvignon", precio: 1900, stock: 10, id: "2", img: "../img/don-david-cabernet.jpg", idCat: "1"},
+    {nombre: "Navarro Correas Colección Privada Malbec", precio: 2200, stock: 3, id: "3", img: "../img/navarro-correas-malbec.jpg", idCat: "2"},
+    {nombre: "Fond de Cave Reserva Malbec", precio: 2130, stock: 10, id: "4", img: "../img/fond-de-cave-reserva-malbec.jpg", idCat: "2"},
+    {nombre: "Rutini Merlot", precio: 4375, stock: 10, id: "5", img: "../img/rutini-merlot.jpg", idCat: "3"},
+    {nombre: "Chateau Vieux Merlot", precio: 3650, stock: 6, id: "6", img: "../img/chateau-vieux-merlot.jpg", idCat: "3"},
+    {nombre: "Demencial Pinot Noir", precio: 3300, stock: 8, id: "7", img: "../img/demencial-pinot-noir.jpg", idCat: "4"},
+    {nombre: "Luigi Bosca Pinot Noir", precio: 4450, stock: 10, id: "8", img: "../img/luigi-bosca-pinot-noir.jpg", idCat: "4"}    
 ]
 
 export const getProductos = () => {
