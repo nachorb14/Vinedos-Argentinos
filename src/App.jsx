492447248992d8c612da9a2d6a0ac407cc1919ef
Ignacio Romero Barolin <nachorb2001@gmail.com>
2023-06-11 23:09:06 -0300

    Agregando Checkout a la página

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout/Checkout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CarritoProvider } from './context/CarritoContext';
 
@@ -18,6 +19,7 @@ function App() {
             <Route path='/item/:idItem' element={<ItemDetailContainer />} />
             <Route path='*' element={<h2>Sitio en Construcción</h2>} />
             <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
           </Routes>
         </CarritoProvider>
       </BrowserRouter>
